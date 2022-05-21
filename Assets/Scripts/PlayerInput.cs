7fbbfbc915721c69b88a793bf02750b195d7d459
Onat Ozdemir <umuthanonat.ozdemir@ogr.iu.edu.tr>
2022-05-21 05:12:13 +0300

    Created Map and Player

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float playerSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
